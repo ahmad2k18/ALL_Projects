d44f577773e9da2c4700157f13a1e9410a5ab5a4
Ahmad Ahsan <ahmadahsan2k18@gmail.com>
2024-05-06 16:45:52 +0500

    new dummy files

---
+++ DUMY/object.js
@@ -0,0 +1,24 @@
+// object defination 
+let std_obj = {
+    firstname : 'ahmad',
+    lastname : 'hassan',
+    age : 23,
+    address : 'karachi',
+    phone : +923117497715,
+    email : 'ahmad@gmail.com',
+
+}
+console.log(std_obj)
+
+// acess way 1
+console.log(std_obj['firstname']);
+
+// acess way 2
+
+console.log(std_obj.firstname);
+
+// add new record
+
+std_obj['email'] = 'ahmadbutt@gmail.com';
+console.log(std_obj);
+
